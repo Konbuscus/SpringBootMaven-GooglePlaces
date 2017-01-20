8b3085d6d8f536472f607f4b2f2804f0729af6b6
Thomas Guyot <guyot.thomas001@gmail.com>
2017-01-20 15:32:18 +0100

    Ajout de trucs dans pom.xml + try-catch

--- src/main/java/com/example/DetailController.java
+++ src/main/java/com/example/DetailController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.springframework.web.bind.annotation.*;
@@ -28,36 +29,40 @@ public class DetailController {
             JSONObject json = new JSONObject(tokener);
             //result.append("<h2>REQUEST:" + requestUrl + "</h2><strong>DEBUG JSON: " + json.toString() + "</strong>");
 
-            if (json.length() > 0) {
-                JSONObject resultJSON = json.getJSONObject("result");
-                result.append("<p>Nom du restaurant : <strong>" + resultJSON.getString("name") + "</strong><img src='" + resultJSON.getString("icon") + "' title='icon'></p>");
-                result.append("<p>URL : <a href='" + resultJSON.getString("url") + "' target='_blank'>Cliquez ici pour ouvrir dans un nouvel onglet (Google Maps)</a></p>");
-                result.append("<p>Adresse : " + resultJSON.getString("formatted_address") + "</p>");
-                result.append("<p>Numéro de téléphone : " + resultJSON.getString("formatted_phone_number") + " (" + resultJSON.getString("international_phone_number") + ")</p>");
+            try {
+                if (json.length() > 0) {
+                    JSONObject resultJSON = json.getJSONObject("result");
+                    result.append("<p>Nom du restaurant : <strong>" + resultJSON.getString("name") + "</strong><img src='" + resultJSON.getString("icon") + "' title='icon'></p>");
+                    result.append("<p>URL : <a href='" + resultJSON.getString("url") + "' target='_blank'>Cliquez ici pour ouvrir dans un nouvel onglet (Google Maps)</a></p>");
+                    result.append("<p>Adresse : " + resultJSON.getString("formatted_address") + "</p>");
+                    result.append("<p>Numéro de téléphone : " + resultJSON.getString("formatted_phone_number") + " (" + resultJSON.getString("international_phone_number") + ")</p>");
 
-                JSONObject openingHours = resultJSON.getJSONObject("opening_hours");
-                JSONArray periods = openingHours.getJSONArray("periods");
-                ArrayList<String> list = new ArrayList<String>();
-                for (int index = 0; index < periods.length(); ++index) {
-                    list.add("<h3>Jour : " + weekDays.get(periods.getJSONObject(index).getJSONObject("open").getInt("day")) + "</h3>");
-                    list.add("<li>Ouverture à : " + periods.getJSONObject(index).getJSONObject("open").getString("time") + "</li>");
-                    list.add("<li>Fermeture à : " + periods.getJSONObject(index).getJSONObject("close").getString("time") + "</li>");
+                    JSONObject openingHours = resultJSON.getJSONObject("opening_hours");
+                    JSONArray periods = openingHours.getJSONArray("periods");
+                    ArrayList<String> list = new ArrayList<String>();
+                    for (int index = 0; index < periods.length(); ++index) {
+                        list.add("<h3>Jour : " + weekDays.get(periods.getJSONObject(index).getJSONObject("open").getInt("day")) + "</h3>");
+                        list.add("<li>Ouverture à : " + periods.getJSONObject(index).getJSONObject("open").getString("time") + "</li>");
+                        list.add("<li>Fermeture à : " + periods.getJSONObject(index).getJSONObject("close").getString("time") + "</li>");
+                    }
+                    String isOpen = (openingHours.getBoolean("open_now")) ? "Oui" : "Non";
+                    StringBuilder daysAndHours = new StringBuilder();
+                    for (String day : list) {
+                        daysAndHours.append(day);
+                    }
+                    result.append("<p>Horaires d'ouverture :<ul>" +
+                            "<li>Ouvert en ce moment : " + isOpen + "</li>" +
+                            "<ul>" + daysAndHours.toString() + "</ul>" +
+                            "</ul></p>"
+                    );
+                } else {
+                    result.append("json is empty");
                 }
-                String isOpen = (openingHours.getBoolean("open_now")) ? "Oui" : "Non";
-                StringBuilder daysAndHours = new StringBuilder();
-                for (String day : list) {
-                    daysAndHours.append(day);
-                }
-                result.append("<p>Horaires d'ouverture :<ul>" +
-                        "<li>Ouvert en ce moment : " + isOpen + "</li>" +
-                        "<ul>" + daysAndHours.toString() + "</ul>" +
-                        "</ul></p>"
-                );
-            } else {
-                result.append("json is empty");
+            } catch (JSONException ex) {
+                result.append(ex.getMessage());
             }
         } catch(IOException e) {
-
+            result.append(e.getMessage());
         }
         return "<h1>RESULTATS</h1>" + result.toString();
     }
